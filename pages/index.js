b644e230e6a26d548b2dcf6e7d8dec32ca8ae135
thevictorwhyte <victordavidwhyte@gmail.com>
2021-12-16 14:54:10 +0100

    added the fonts

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,6 @@ export default function Home() {
       <Head>
         <title>Victor Whyte</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
-          ref="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap"
-          ref="stylesheet"
-        />
       </Head>
       <Header
         navRef={navRef}
